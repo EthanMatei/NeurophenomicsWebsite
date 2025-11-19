117717b5afe06653bde8c2a06801209ba95c4cfb
jbalducci <jbalducci@arizona.edu>
2025-11-19 14:47:13 -0700

    Added New C4P Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import logo from '../assets/UofA.png';
+import c4pLogo from '../assets/C4P_Logo.png';
 
 const SidebarContainer = styled.nav`
   width: 175px;           /* Fixed width for sidebar */
   min-height: 100vh;      /* Full viewport height */
   display: flex;
   flex-direction: column; /* Stack items vertically */
-  align-items: flex-start;
+  justify-content: space-between; /* Push top and bottom content apart */
   background-color: #2c3e50;
   padding: 1rem;
-  position: flex;        /* Keep it fixed on the left if you want */
-  top: 0;
-  left: 0;
   color: white;
+`;
+
+const TopSection = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 const BrandContainer = styled.div`
@@ -24,7 +27,7 @@ const BrandContainer = styled.div`
 `;
 
 const Logo = styled.img`
-  width: 40px;    /* Slightly bigger, tweak as needed */
+  width: 40px;    /* Adjust as needed */
   height: auto;
   margin-right: 10px;
 `;
@@ -33,8 +36,6 @@ const Title = styled.h1`
   margin: 0;
   font-size: 1rem;
   line-height: 1.2;
-  /* Because the sidebar can be narrower, 
-     reduce font-size if needed */
 `;
 
 const NavLinks = styled.ul`
@@ -57,49 +58,60 @@ const StyledLink = styled(Link)`
   }
 `;
 
+const BottomLogo = styled.img`
+  width: 50px;    /* Adjust as needed */
+  height: auto;
+  align-self: center; /* Center horizontally at bottom */
+`;
+
 function Sidebar() {
   return (
     <SidebarContainer>
-      <BrandContainer>
-        <Logo src={logo} alt="Logo" />
-        <Title>Center for Precise, Personalized, and Preventive Psychiatry (C4P)</Title>
-      </BrandContainer>
+      <TopSection>
+        <BrandContainer>
+          <Logo src={logo} alt="University Logo" />
+          <Title>Center for Precise, Personalized, and Preventive Psychiatry (C4P)</Title>
+        </BrandContainer>
 
-      <NavLinks>
-        <NavLinkItem>
-          <StyledLink to="/">Home</StyledLink>
-        </NavLinkItem>
-        <NavLinkItem>
-          <StyledLink to="/selected-publications">Publications</StyledLink>
-        </NavLinkItem>
-        <NavLinkItem>
-          <StyledLink to="/media">Media</StyledLink>
-        </NavLinkItem>
-        <NavLinkItem>
-          <StyledLink to="/Our-Lab">People</StyledLink>
-        </NavLinkItem>
-        <NavLinkItem>
-          <StyledLink to="/Firsts">Firsts</StyledLink>
-        </NavLinkItem>
-        <NavLinkItem>
-          <StyledLink to="/FAQ">FAQ</StyledLink>
-        </NavLinkItem>
-        <NavLinkItem>
-          <StyledLink to="/contactus">Contact Us</StyledLink>
-        </NavLinkItem>
-        <NavLinkItem>
-          <StyledLink to="/gscholar">Citations and Reproducibility</StyledLink>
-        </NavLinkItem>
-        <NavLinkItem>
-          <StyledLink to="/book">Life and Mind Book</StyledLink>
-        </NavLinkItem>
-        <NavLinkItem>
-          <StyledLink to="/philan">Philanthropy</StyledLink>
-        </NavLinkItem>
-        <NavLinkItem>
-          <StyledLink to="/photoalbum">Photo Album</StyledLink>
-        </NavLinkItem>
-      </NavLinks>
+        <NavLinks>
+          <NavLinkItem>
+            <StyledLink to="/">Home</StyledLink>
+          </NavLinkItem>
+          <NavLinkItem>
+            <StyledLink to="/selected-publications">Publications</StyledLink>
+          </NavLinkItem>
+          <NavLinkItem>
+            <StyledLink to="/media">Media</StyledLink>
+          </NavLinkItem>
+          <NavLinkItem>
+            <StyledLink to="/Our-Lab">People</StyledLink>
+          </NavLinkItem>
+          <NavLinkItem>
+            <StyledLink to="/Firsts">Firsts</StyledLink>
+          </NavLinkItem>
+          <NavLinkItem>
+            <StyledLink to="/FAQ">FAQ</StyledLink>
+          </NavLinkItem>
+          <NavLinkItem>
+            <StyledLink to="/contactus">Contact Us</StyledLink>
+          </NavLinkItem>
+          <NavLinkItem>
+            <StyledLink to="/gscholar">Citations and Reproducibility</StyledLink>
+          </NavLinkItem>
+          <NavLinkItem>
+            <StyledLink to="/book">Life and Mind Book</StyledLink>
+          </NavLinkItem>
+          <NavLinkItem>
+            <StyledLink to="/philan">Philanthropy</StyledLink>
+          </NavLinkItem>
+          <NavLinkItem>
+            <StyledLink to="/photoalbum">Photo Album</StyledLink>
+          </NavLinkItem>
+        </NavLinks>
+      </TopSection>
+
+      {/* C4P logo at the bottom */}
+      <BottomLogo src={c4pLogo} alt="C4P Logo" />
     </SidebarContainer>
   );
 }
